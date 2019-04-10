fcfd2b319fe8aa3888ec9b56fd66f9e74f1e109d
SAO <1053001374@qq.com>
2019-04-10 16:13:28 +0800

    简单实现了从邮件文件中搜索所有主题和发件人并输出。

--- 第23章习题/23-3/源.cpp
+++ 第23章习题/23-3/源.cpp
@@ -1,1 +1,38 @@
-ccc
+#include<string>
+#include<vector>
+#include<fstream>
+#include<iostream>
+#include<regex>
+using namespace std;
+//------------------------------------------------------------------------------
+int main()
+{
+	string file_name = "my-mail-file.txt";
+	ifstream in(file_name.c_str());    // open the file
+	if (!in) {
+		cerr << "no " << file_name << '\n';
+		system("pause");
+	}
+	string s;
+	vector<string> senders;
+	vector<string> subjects;
+	regex sender{ R"(^Subject:([[:print:]]*)$)" };
+	regex subject{ R"(^From:([[:print:]]*)$)" };
+	while (getline(in, s))
+	{
+		smatch matches;
+		if (regex_match(s, matches, sender))
+			senders.push_back(matches[0]);
+		else if (regex_match(s, matches, subject))
+			subjects.push_back(matches[0]);
+	}
+	for (string s : senders)
+		cout << s << endl;
+	cout << "-----------------------------\n";
+	for (string s : subjects)
+		cout << s << endl;
+	system("pause");
+	return 0;
+}
+
+//------------------------------------------------------------------------------
